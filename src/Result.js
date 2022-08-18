b6557244acb0bfff51b5e1ff2cba2b6701d60a80
mohitsahu19 <mohitcsahu@gmail.com>
2022-08-18 18:43:51 +0530

    first commit

---
+++ src/Result.js
@@ -0,0 +1,61 @@
+import React from 'react'
+
+const Result = ({word ,phonetics ,meanings ,setText}) => {
+  return (
+    <ul>
+      <li className="word">
+        <h2>{word}</h2>
+        {
+          phonetics.map((phonetics ,index ) =>(
+            <span key ={index}>{phonetics.text}</span>
+          ))
+        }
+        
+
+      </li>
+
+      {
+        meanings.map((meaning, index) =>(
+          <li className="contain">
+            <h3>noun</h3>
+    
+            <div className="details meaning">
+              <h3>Meaning</h3>
+              {
+                meaning.definitions.map((definition, index) => (
+                  <p key={index}>- {definition.definition}</p>
+
+                ))
+              }
+            </div>
+
+            {
+              meaning.synonyms.length !== 0&&
+              <div className="details synonyms">
+                <h3>synonyms</h3>
+                {
+                  meaning.synonyms.map((synonym, index) =>(
+                    <span key={index}
+                    onClick={()=>setText(synonym)}>
+                      {`${synonym}, `},
+                    </span>
+
+                  ))
+                }
+              
+              
+            </div>
+            }
+        </li>         
+
+        ))
+      }
+
+    
+      
+
+    </ul>
+  )
+}
+
+export default Result
